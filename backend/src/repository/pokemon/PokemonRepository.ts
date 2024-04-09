5d0e83d271d7f297d4373bd8b2175e1192d352a8
Tim Fehrmann <tim.fehrmann@aclue.de>
2024-04-09 14:58:30 +0200

    removed deprecated backend code

--- backend/src/repository/pokemon/PokemonRepository.ts
+++ backend/src/repository/pokemon/PokemonRepository.ts
@@ -2,8 +2,6 @@ import IPokemon from "../../../type/frontend/pokemon/IPokemon";
 import pokemonNames from "./PokemonNames";
 import PokemonNames from "./PokemonNames";
 
-// const pokemonNamePermutations = generatePermutations(pokemonNames);
-// const shuffledPokemonNamePermutations = shuffleArray(pokemonNames);
 const Pokemon = pokemonNames.map((name, id) => ({name, id}));
 const PokemonRepository: { Pokemon: IPokemon[] } = {
     Pokemon,
@@ -12,7 +10,6 @@ console.info(`Created ${PokemonRepository.Pokemon.length} name permutations from
 
 export function getPokemonDb(): IPokemon[] | never {
     return PokemonRepository.Pokemon.slice(0, 100);
-    // return PokemonRepository.Pokemon;
 }
 
 export function savePokemonDb(pokemon: IPokemon[]): void {
